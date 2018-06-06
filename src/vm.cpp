ec6541152fc3b0e6866e58897e4347e855db834d
FacelessJ <bitbucket@facelessj.com>
2018-06-06 12:32:50 +1000

    Add CB SLA, SRA commands

--- src/vm.cpp
+++ src/vm.cpp
@@ -762,6 +762,24 @@ namespace gb_emu
 			}
 				break;
 			case Opcode_Prefix_Misc1_Command_Groups::SHIFT:
+			{
+				uint8_t val = readValue(reg);
+				clearFlags();
+				if(bit3) // Shift right
+				{
+					setFlag(Flag::C, val & 0x01);
+					val >> 1;
+					if(val & 0x40) // Prevent MSB from changing
+						val |= 0x80;
+				}
+				else
+				{
+					setFlag(Flag::C, val & 0x80);
+					val << 1;
+				}
+				writeValue(reg, val);
+				setFlag(Flag::Z, val);
+			}
 				break;
 			case Opcode_Prefix_Misc1_Command_Groups::SWAP_SHIFT: // Swap/Shift Right, clear MSB
 				break;
