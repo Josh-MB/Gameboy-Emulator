e5399873785670e98a4599cf96db9b9df37b2155
FacelessJ <bitbucket@facelessj.com>
2018-07-01 12:28:43 +1000

    Add SDL2

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
 #include "../include/vm.hpp"
+#include <SDL.h>
 
-int main()
+int main(int argc, char *args[])
 {
+	SDL_Init(SDL_INIT_VIDEO);
+
+	SDL_Window* window = SDL_CreateWindow("SDL2Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+
+	auto renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+	SDL_SetRenderDrawColor(renderer, 128, 0, 0, SDL_ALPHA_OPAQUE);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+
+	SDL_Delay(3000);
+
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
 	gb_emu::VM vm;
 	vm.run();
 	return 0;
